f4c2a953234691c29aec6593d082264030f226e9
TWhilst <tochethestrict@gmail.com>
2025-05-04 21:46:16 +0100

    edited script.sh and create-rds-instance.yml

--- script.sh
+++ script.sh
@@ -8,6 +8,6 @@ sudo yum -y install php
 sudo systemctl start mariadb
 yes -n | sudo mysql_secure_installation
 sudo yum -y install git
-sudo git clone https://github.com/CeeyIT-Solutions/php-lamp3.git /var/www/html
+sudo git clone -b php https://github.com/TWhilst/BackedUpPhpApp.git /var/www/html
 sudo yum -y install php php-mysqlnd
 sudo systemctl start httpd
